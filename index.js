eec81eeeba5940d78bde03e00a7c9fb07a6e61bc
Peter Kim <peteryunkim@gmail.com>
2017-01-12 18:06:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested')
+}
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div div div div')
+}
+
+function increaseRankBy(n){
+  var lane = document.querySelectorAll('ranked-list')
+  for (let i = 0, k = lane.length; i < k; i++){
+    lane[i].innerHTML = (i + n).toString()
+  }
+  return document.querySelectorAll('ranked-list')
+}
